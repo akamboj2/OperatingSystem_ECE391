84adb2a88527bf0be15531c7b53eba9219a9fee4
unknown <raghava4@illinois.edu>
2019-03-10 04:35:59 -0500

    IDT init

---
+++ student-distrib/idt.h
@@ -0,0 +1,29 @@
+#ifndef _IDT
+#define _IDT
+
+void idt_init();
+
+void add_vector(int index, void *handler, int dpl, int type);
+
+void divide_err();
+void nmi();
+void breakpoint();
+void overflow();
+void bound();
+void invalid_opcode();
+void device_NA();
+void double_fault();
+void coproessor_seg_overrun();
+void invalid_tss();
+void seg_not_present();
+void stack_seg_fault();
+void general_protection();
+void page_fault();
+void floating_point_err();
+void alignment_check();
+void machine_check();
+void floating_point_exception();
+void system_calls();
+void rtc_interrupt();
+void keyboard_interrupt();
+#endif

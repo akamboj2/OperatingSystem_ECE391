ef25a5e03cff2606525e735905d31ff270ec739e
Vinith <vinithr2@illinois.edu>
2019-04-06 21:04:14 -0500

    write progress

--- student-distrib/sys_calls.c
+++ student-distrib/sys_calls.c
@@ -32,10 +32,14 @@ int32_t execute (const uint8_t* command){
  * Side Effects: none
  */
 int32_t read(int32_t fd, void* buf, int32_t nbytes){
+  if(buf == NULL || fd > 7 || fd < 0)
+    return -1;
+
   int32_t position = file_array[fd].fops_table->read(fd, buf, nbytes);
   if(position == 0 || position == -1)
     return 0;
   file_array[fd].f_pos += position;
+
   return position;
 }
 
@@ -46,7 +50,14 @@ int32_t read(int32_t fd, void* buf, int32_t nbytes){
  * Side Effects: none
  */
 int32_t write (int32_t fd, const void* buf, int32_t nbytes){
-  return 0;
+  if(buf == NULL || fd > 7 || fd < 0)
+    return -1;
+
+  int32_t position = file_array[fd].fops_table->write(fd, buf, nbytes);
+
+  if(position == -1)
+    return 0;
+  return position;
 }
 
 /*open
